7fecb2aa20ed4e18c6b31706d94c7711efc7d65e
Sami Sikkilä <sami.sikkila@metropolia.fi>
2020-11-17 21:06:44 +0200

    props

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => (
-    <div>
-        <p>Hello world</p>
-    </div>
-)
+const Hello = (props) => {
+    return (
+        <div>
+            <p>Hello world {props.name}, you are {props.age} years old</p>
+        </div>
+    )
+}
+
+const App = () => {
+    const nimi = 'Pekka'
+    const ika = 10
+
+    return (
+        <div>
+            <h1>Greetings</h1>
+            <Hello name="Maya" age={26 + 10}/>
+            <Hello name={nimi} age={ika}/>
+        </div>
+    )
+}
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
